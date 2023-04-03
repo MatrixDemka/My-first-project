71b27652fb559175dbfdcc5866cc7103e87757ee
Aleksey_Demin <matrixdemka@gmail.com>
2023-04-03 09:58:57 +0300

    the function of sorting and quick merging lists

--- my_learning/merge_list.py
+++ my_learning/merge_list.py
@@ -1,7 +1,10 @@
-def quick_merge(list1, list2):
-    return sorted(list1+list2)
+def merge(list1, list2):
+    result = list1 + list2
+    result.sort()
+
+    return result
 
 
 list1 = [0, 4, 7, 79, 43, 55, 8, 2, 3, 39, 100, 121, 668, 14, 18, 15]
 list2 = [1, 10, 11, 13, 21, 25, 65, 90, 97, 51, 27, 84, 336, 569, 137, 73]
-print(quick_merge(list1, list2))
+print(merge(list1, list2))
